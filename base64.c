020a3577879df85a7b8fb1cea02e7945873ddc85
nyux <emilia.emerald@gmail.com>
2014-08-12 11:39:21 -0600

    got rid of compiler warnings; buggy output

--- base64.c
+++ base64.c
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define FIRST_CHAR((x)) *(x)
-#define SECOND_CHAR((x)) *((x) + 1)
-#define THIRD_CHAR((x)) *((x) + 2)
+#define FIRST_CHAR(x) *(x)
+#define SECOND_CHAR(x) *((x) + 1)
+#define THIRD_CHAR(x) *((x) + 2)
 
 void base64_convert(char a, char b, char c)
 {
@@ -21,11 +21,11 @@ int main(void)
     char *hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
     int hexlen = strlen(hex);
-    int[] padding = {-1, -1};
+    int padding[] = {-1, -1};
     int i;
 
     
-    for (i = 0; i < hexlen - hexlen%3; i += 3, hex = *(hex + 3))
+    for (i = 0; i < hexlen - hexlen%3; i += 3, hex += 3)
     {
        base64_convert(FIRST_CHAR(hex), SECOND_CHAR(hex), THIRD_CHAR(hex));
     }
