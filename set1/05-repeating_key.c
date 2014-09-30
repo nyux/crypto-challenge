1d83bbb09d915652a33f3c7f266a6e95ea28f2c7
nyux <emilia.emerald@gmail.com>
2014-09-29 18:52:17 -0600

    filled in implementation; there is some extra cruft at the end of the hexstring, need to figure out where the error is

--- set1/05-repeating_key.c
+++ set1/05-repeating_key.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utility/utility.h"
 
+char* repeating_key_encode(char *bytestr, size_t bytestr_len, char *key,
+        size_t keylen)
+{
+    char *xor_bytestr = malloc(bytestr_len);
+
+    if (!xor_bytestr) utility_malloc_error();
+
+    memcpy(xor_bytestr, bytestr,  bytestr_len);
+
+    for (int i = 0; i < bytestr_len; i++)
+    {
+        xor_bytestr[i] ^= key[i % keylen];
+    }
+
+    return xor_bytestr;
+}
+
+
+
 int main(void)
 {
-    return 0;
+    char *teststr = "Burning 'em, if you ain't quick and nimble", *key = "ICE";
+    char *raw_result = repeating_key_encode(teststr, strlen(teststr), key, 3);
+    char *result;
+
+    if (raw_result)
+    {
+        result = utility_bytestr_to_hexstr(raw_result, strlen(teststr));
+
+        if (result)
+        {
+            printf("%s\n", result);
+            free(result);
+        }
+
+        free(raw_result);
+    }
 }
